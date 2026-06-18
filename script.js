5c5f3ddff7a494c81328eb96f772bfeb34e16674
ShristiDuhhon <shristiduhoon24@gmail.com>
2026-06-18 14:46:21 +0530

    first

---
+++ script.js
@@ -0,0 +1,17 @@
+function addToCart(name, price)
+{
+    let cart =
+    JSON.parse(localStorage.getItem("cart")) || [];
+
+    cart.push({
+        name:name,
+        price:price
+    });
+
+    localStorage.setItem(
+        "cart",
+        JSON.stringify(cart)
+    );
+
+    alert("Product Added!");
+}
